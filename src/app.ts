4787964bdd857cf76d87a01342cea5fc80acb8aa
Juan Sebastian Rondon Lopez <jsebas3102@hotmail.com>
2024-03-10 15:58:57 -0500

    Captura de errores y retorno del codigo qr

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import {addKeyword, createBot, createFlow, createProvider, MemoryDB} from '@bot-whatsapp/bot'
+import {addKeyword, createBot, createFlow, createProvider, EVENTS, MemoryDB} from '@bot-whatsapp/bot'
 import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
+import { ServerResponse } from 'http';
+import fs from 'fs'
 
-const flowBienvenida = addKeyword('hola').addAnswer('Buenas!! Bienvenido')
+const flowBienvenida = addKeyword(['Hola', 'Buen día']).addAnswer(['Buenas, bienvenido a mi chat', '¿Como puedo ayudarte?'])
 
 
 
@@ -11,16 +13,41 @@ const main = async () => {
 
     provider.initHttpServer(3002)
 
-    provider.http.server.post('/send-message', handleCtx(async (bot, req, res) => {
-        const body = req.body
-        const phone = body.phone
-        const msg = body.msg
-        await bot.sendMessage(phone, msg, {})
-        res.end('El mensaje se envió correctamente')
+    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
+        try{
+            const body = req.body
+            const phone = body.phone
+            const msg = body.msg
+            await bot.sendMessage(phone, msg, {})
+        } catch(error){
+            res.statusCode = 400
+            res.end('No se ha podido enviar el mensaje. Puede que el proveedor no este autenticado.')
+        }
+       
+    }))
+
+    provider.http?.server.get('/get-qr', handleCtx(async (bot, req, res) => {
+        const rutaQR = 'bot.qr.png'
+
+        //Verificamos si existe la imagen
+        fs.access(rutaQR, fs.constants.F_OK, (err) => {
+            if (err) {
+                // Si hay un error (por ejemplo, el archivo no existe), enviar una respuesta 404
+                res.statusCode = 404;
+                res.end('Imagen no encontrada');
+                return;
+            }
+
+             // Si el archivo existe, establecer los encabezados de respuesta adecuados
+             res.setHeader('Content-Type', 'image/png');
+
+             // Leer el archivo de imagen y enviarlo como respuesta
+            fs.createReadStream(rutaQR).pipe(res as ServerResponse);
+        })
     }))
 
     await createBot({
-        flow: createFlow([]),
+        flow: createFlow([flowBienvenida]),
         database: new MemoryDB(),
         provider: provider
     })
